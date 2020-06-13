0445800a2b02276ea76b7854b890db14e2fece84
zach chung <chungchunluk@gmail.com>
2020-06-13 13:36:46 +0800

    hide pw

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -71,8 +71,8 @@ Rails.application.configure do
     address: "smtp.gmail.com",
     port: 587,
     domain: "gmail.com",
-    user_name: "coursecreator404@gmail.com",
-    password: "walucjdfiogfhttb",
+    user_name: ENV['GMAIL_ADDRESS'],
+    password: ENV['GMAIL_APP_PASSWORD'],
     authentication: "plain",
     enable_starttls_auto: true
   }
