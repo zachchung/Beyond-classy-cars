f0950f626c162c0db12310cdaa8e3fdda3c167a3
zach chung <chungchunluk@gmail.com>
2020-06-14 02:01:27 +0800

    try deploy without active cable

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -5,11 +5,11 @@ class MessagesController < ApplicationController
     @message.chatroom = @chatroom
     @message.user = current_user
     if @message.save
+      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
       # when got new msg, ask channel to SEND BROADCAST (so no need to refresh the page to see the msg)
       ChatroomChannel.broadcast_to(@chatroom,
         render_to_string(partial: "messages/message", locals: { message: @message })
       ) # can't render twice in controller. Use render_to_string instead (eg. map- render_to_string "info-windows).
-      redirect_to chatroom_path(@chatroom, anchor: "message-#{@message.id}")
     else
       render "chatrooms/show" # the form is from chatrooms_controller#show
     end
