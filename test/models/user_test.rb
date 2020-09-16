498849faeddfda46a98279d75cef9d909f90b7e4
zach chung <chungchunluk@gmail.com>
2020-09-16 17:08:38 +0800

    added unit test

--- test/models/user_test.rb
+++ test/models/user_test.rb
@@ -4,4 +4,10 @@ class UserTest < ActiveSupport::TestCase
   # test "the truth" do
   #   assert true
   # end
+  test "#fullname returns user full name" do
+    user = User.new(first_name: "John", last_name: "Lenon")
+    actual = user.fullname
+
+    assert_equal("John Lenon", actual)
+  end
 end
